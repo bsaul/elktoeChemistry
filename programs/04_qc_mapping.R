08901ec3d9c48a18fd026be4bf7adf89c78f3d6b
bsaul <bradleysaul@gmail.com>
2018-11-11 09:29:59 -0500

    much clean up and schematizing of the valve alignment QC process

--- programs/04_qc_mapping.R
+++ programs/04_qc_mapping.R
@@ -5,69 +5,86 @@
 # Purpose: 
 #-----------------------------------------------------------------------------#
 
-library(ggplot2)
 
-test1 <- create_wide_analysis_data(
-  valve_data, 
- .reference_transition = "on_", 
- .zero_function = identity) %>%
-  mutate(idref_method = "default")
+# Testing various methods of identifying the reference point from which to
+# calculate distances
 
-test2 <- create_wide_analysis_data(
-  valve_data, 
-   .reference_transition = "ipx_", 
-   .zero_function = ca_changepoint, 
-   .zf_args = list(.use_up_to_row = 150, .method = "AMOC")) %>%
-  mutate(idref_method = "ca_changepoint")
-
-test3 <- create_wide_analysis_data(
-  valve_data, 
-   .reference_transition = "ipx_", 
-   .zero_function = pbca_changepoint, 
-   .zf_args = list(.use_up_to_row = 150, .method = "AMOC")) %>%
-  mutate(idref_method = "pbca_changepoint")
-
-test4 <- create_wide_analysis_data(
-  valve_data, 
-  .reference_transition = "ipx_", 
-  .zero_function = pbca_maxpoint, 
-  .zf_args = list(.use_up_to_row = 150)) %>%
-  mutate(idref_method = "pbca_maxpoint")
-
-tests <- bind_rows(test1, test2, test3, test4) %>%
-  mutate(
-    idref_method = factor(idref_method, levels = c("default", "ca_changepoint", "pbca_changepoint", "pbca_maxpoint"))
+test_methods <- list(
+  A = list(
+    rt = "on_",
+    zf = identity,
+    zfa = list()
+  ),
+  B = list(
+    rt = "ipx_",
+    zf = id_changepoint,
+    zfa = list(.var = "Ca43_CPS", .use_up_to_row = 150, .method = "AMOC", .outer = FALSE)
+  ),
+  C = list(
+    rt = "_opx",
+    zf = id_changepoint,
+    zfa = list(.var = "Ca43_CPS", .use_up_to_row = 150, .method = "AMOC", .outer = TRUE)
+  ),
+  D = list(
+    rt = "ipx_",
+    zf = id_changepoint,
+    zfa = list(.var = "Pb208_CPS", .use_up_to_row = 150, .method = "AMOC", .outer = FALSE)
+  ),
+  E = list(
+    rt = "_opx",
+    zf = id_changepoint,
+    zfa = list(.var = "Pb208_CPS", .use_up_to_row = 150, .method = "AMOC", .outer = TRUE)
+  ),
+  `F` = list(
+    rt = "ipx_",
+    zf = pbca_minpoint,
+    zfa = list(.use_up_to_row = 150, .outer = FALSE)
+  ),
+  G = list(
+    rt = "_opx",
+    zf = pbca_minpoint,
+    zfa = list(.use_up_to_row = 150, .outer = TRUE)
+  ),
+  H = list(
+    rt = "ipx_",
+    zf = maxpoint,
+    zfa = list(.var = "ratio", .use_up_to_row = 150, .outer = FALSE)
+  ),
+  I = list(
+    rt = "_opx",
+    zf = maxpoint,
+    zfa = list(.var = "ratio", .use_up_to_row = 150, .outer = TRUE)
+  ),
+  J = list(
+    rt = "ipx_",
+    zf = maxpoint,
+    zfa = list(.var = "Pb208_CPS", .use_up_to_row = 150, .threshold = 1000, .outer = FALSE)
+  ),
+  K = list(
+    rt = "_opx",
+    zf = maxpoint,
+    zfa = list(.var = "Pb208_CPS", .use_up_to_row = 150, .threshold = 1000,  .outer = TRUE)
   )
+)
 
-valve_lengths <- valve_measurements %>%
-  group_by(id, transect) %>%
-  filter(sum(grepl("ipx_", layer_transition)) > 0, sum(grepl("opx_", layer_transition)) > 0) %>%
-  summarise(
-    on_opx_distance = distance[grepl("opx_", layer_transition)],
-    on_ipx_distance = distance[grepl("ipx_", layer_transition)], 
-    valve_length = on_opx_distance - on_ipx_distance
-  ) 
-
-
-tester <- function(data){
-  data %>%
-    group_by(idref_method, id, transect) %>%
-    filter(sum(layer == "ipx") > 0, sum(layer == "opx") > 0) %>%
-    summarise(
-      chem_length = distance[min(which(layer == "opx"))] - distance[max(which(layer == "ipx"))]
-    ) %>% 
-    inner_join(valve_lengths, by = c("id", "transect")) %>%
-    mutate(diff = chem_length - valve_length)
+apply_method <- function(.l, .n){
+  create_wide_analysis_data(
+    valve_data, 
+    .reference_transition = .l$rt, 
+    .zero_function        = .l$zf,
+    .zf_args              = .l$zfa) %>%
+   mutate(idref_method = .n)
 }
 
-test_check <- tests %>% tester %>% dplyr::filter(diff > -250) 
+## Apply all the methods
+method_tests <- purrr::map2_dfr(
+  test_methods, names(test_methods), 
+  ~ apply_method(.x, .y)) %>%
+  mutate(idref_method = factor(idref_method))
 
-
-### Plot checks ####
-
-
+### Plot Methods ####
 create_qc_plot <- function(.idt){
-  hold <- tests %>%
+  hold <- method_tests %>%
     group_by(idref_method) %>%
     mutate(
       PbCa_ratio = log10(abs(Pb208_CPS)/abs(Ca43_CPS)), 
@@ -101,72 +118,91 @@ create_qc_plot <- function(.idt){
     )
 }
 
-ids <- distinct(test_check, id, transect) %>% mutate(idt = paste(id, transect, sep = "_")) %>% pull(idt)
+ids <- distinct(method_tests, id, transect) %>% mutate(idt = paste(id, transect, sep = "_")) %>% pull(idt)
 invisible(lapply(ids, function(id){
   p <- create_qc_plot(id) 
-  ggsave(sprintf("programs/QC/%s.png", id), p, width = 5, height =  2, dpi = 150, units = "in")
+  ggsave(sprintf("programs/QC/%s.png", id), p, width = 5, height =  3, dpi = 150, units = "in")
 }))
 
 
-create_qc_plot("C472_2") 
+## Compare valve lengths measured to valve lengths auto detected ####
 
-test3 %>% filter(id == "A3", transect == "1") %>% pull(cpt)
+# Calculate the valve lengths as measured
+valve_lengths <- valve_measurements %>%
+  group_by(id, transect) %>%
+  filter(sum(grepl("ipx_", layer_transition)) > 0, sum(grepl("opx_", layer_transition)) > 0) %>%
+  summarise(
+    on_opx_distance = distance[grepl("_opx", layer_transition)],
+    on_ipx_distance = distance[grepl("ipx_", layer_transition)], 
+    valve_length = on_opx_distance - on_ipx_distance
+  )  %>%
+  select(id, transect, valve_length)
 
-library(pracma)
-temp1 <- tests %>%
-  filter(id == "C475", transect == "2", idref_method == "pbca_changepoint") %>%
-  filter(row_number() <= 150) %>%
+# For each method, detect the outer valve edge
+
+tm <- test_methods[2:length(test_methods)]
+tm_lgl <- purrr::map_lgl(tm, ~ .x$zfa$.outer)
+
+chem_valve_lengths <- method_tests %>% 
+  filter(idref_method != "A") %>%
+  group_by(idref_method, id, transect) %>%
+  summarise(
+    outer_edge_rn = min(which(layer == "opx"), na.rm = TRUE),
+    inner_edge_rn = max(which(layer == "ipx"), na.rm = TRUE)
+  )  %>%
   mutate(
-    rn = row_number(),
-    PbCa_ratio = Pb208_CPS/Ca43_CPS
-  ) 
-temp1
+    use_inner = idref_method %in% names(tm)[!tm_lgl],
+    use_outer = idref_method %in% names(tm)[tm_lgl],
+    groupings = case_when(
+      idref_method %in% c("B", "C") ~ "B-C",
+      idref_method %in% c("D", "E") ~ "D-E",
+      idref_method %in% c("F", "G") ~ "F-G",
+      idref_method %in% c("H", "I") ~ "H-I",
+      idref_method %in% c("J", "K") ~ "J-K"
+    )
+  ) %>%
+  group_by(groupings, id, transect) %>%
+  summarise(
+    inner_edge_rn = inner_edge_rn[use_inner],
+    outer_edge_rn = outer_edge_rn[use_outer]
+  ) %>%
+  mutate(
+    valve_obs = outer_edge_rn - inner_edge_rn,
+    auto_detect_valve_length = valve_obs * 2.881
+  ) %>%
+  select(groupings, id, transect, auto_detect_valve_length)
 
-zz <- max(findpeaks(temp1$PbCa_ratio, threshold = 1)[, 2])
 
-zz <- nrow(temp1) - changepoint::cpt.mean(rev(cummin(temp1$PbCa_ratio)))@cpts[1]
-zz <- changepoint::cpt.meanvar(cumsum(rev(abs(diff(c(rep(temp1$PbCa_ratio[1], 100), temp1$PbCa_ratio))))))@cpts[1] - 100
-zz <- changepoint::cpt.var(diff(temp1$PbCa_ratio))@cpts[1]
-
-zz
-zz <- which(diff(temp1$PbCa_ratio) == min(diff(temp1$PbCa_ratio)))
-zz
-plot(diff(temp1$PbCa_ratio))
-
-temp1 %>%
-  ggplot(. , aes(x = rn, y = PbCa_ratio)) + 
-  geom_line() + 
-  geom_vline(
-    xintercept = zz
+## Compare valve lengths ####
+valve_length_compare <- chem_valve_lengths %>%
+  left_join(valve_lengths, by = c("id", "transect")) %>%
+  mutate(
+    difference = auto_detect_valve_length - valve_length
   )
 
 
-
-temp2 <- create_wide_analysis_data(
-  valve_data %>% filter(id == "A3", transect == "1"), 
-  .reference_transition = "ipx_", 
-  .zero_function = pbca_changepoint, 
-  .zf_args = list(.use_up_to_row = 150, .method = "AMOC")) 
-
-
-%>%
-  pull(cpt)
-
-# # test1 %>%
-#   # lengths %>%
-# valve_measurements %>%
-#   dplyr::filter(id == "C497", transect == "2") %>%
-#   View()
+ggplot(
+  valve_length_compare %>% filter(difference > -500),
+  aes(x = difference, y = groupings)
+) + 
+  geom_vline(xintercept = 0) +
+  geom_jitter() 
 
 
+valve_data %>%
+  filter(id == "C") %>%
+  pull(measures)
+create_qc_plot("C485_1") 
+create_qc_plot("C502_2") 
+create_qc_plot("A1_1") 
 
-hist(test1_check$diff, breaks = 30)
-hist(test2_check$diff, breaks = 30)
-hist(test3_check$diff, breaks = 30)
-hist(test4_check$diff, breaks = 30)
 
-mean(test1_check$diff)
+### bombing range ####
+x <- valve_data %>%
+  filter(id == "A1", transect == "1") %>%
+  pull(chemistry)
 
-
+x <- x[[1]]
+pracma::findpeaks(x$Pb208_CPS[1:150], threshold = 10000)
 
 
