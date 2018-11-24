2a70df9059bfcd1f0a5f6a447de09133888436e3
bsaul <bradleysaul@gmail.com>
2018-11-24 12:48:40 -0500

    creates a dataset of annual layer availability

--- programs/05_create_annual_layer_availability_data.R
+++ programs/05_create_annual_layer_availability_data.R
@@ -5,16 +5,15 @@
 # Purpose:
 #-----------------------------------------------------------------------------#
 
-
-valve_annual_layer_availability <- valve_analysis %>%
+valve_annual_layer_availability <- valve_data %>%
+  select(id, transect, distance) %>%
+  tidyr::unnest() %>%
   group_by(id, transect) %>%
-  filter(layer == "nacreous") %>%
+  filter(layer == "ncr") %>%
   group_by(annuli, add = TRUE) %>%
   summarise(n = n() > 1) %>%
   tidyr::spread(
     key = annuli, value = n, fill = FALSE
   ) 
-
-
 
 saveRDS(valve_annual_layer_availability, file = 'data/valve_annual_layer_availability.rds')
