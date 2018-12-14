a7e26e465e6ba10d7585e7615ce491c67910265f
bsaul <bradleysaul@gmail.com>
2018-12-14 10:38:57 -0500

    clean up

--- programs/00_setup_and_create_data.R
+++ programs/00_setup_and_create_data.R
@@ -1,5 +1,5 @@
 #-----------------------------------------------------------------------------#
-#   Title: Import mussel shell data
+#   Title: Import mussel shell data and create analysis file
 #  Author: B Saul
 #    Date: 2016-03-05
 # Purpose: Load and save data objects for analysis
@@ -18,7 +18,7 @@ exclude_files <- c("README",
                    "2-A4-4", "8-C474-5", "27-C531-2", # Horizontal transects
                    "11-C482-1", "11-C483-1", 
                    "16-C497-1", "16-C497-2", "16-C497-3", # Excluding C497 due to unusual structure at the mantle edge
-                   "45-P139-2" # ??? Per notes: This shell does not exhist.
+                   "45-P139-2" # ??? Per notes: This shell does not exist.
 )
 
 ###
