93ae699215f6ebb3913d52806dce95a025358af7
bsaul <bradleysaul@gmail.com>
2019-01-19 17:45:05 -0500

    first analysis

--- programs/10a_analysis_functions.R
+++ programs/10a_analysis_functions.R
@@ -8,6 +8,15 @@ library(ggplot2)
 library(dplyr)
 valve_data <- readRDS("data/valve_data.rds")
 
+residFUN <- function(x) {
+  residuals(lm(x ~ c(0, x[-length(x)])))
+}
+
+valve_data <- valve_data %>%
+  mutate(
+    chemistry = purrr::map(chemistry, ~  mutate_all(.x, .funs = funs(residFUN)))
+  )
+
 
 make_valve_filter <- function(valve_data){
   
@@ -143,13 +152,13 @@ plot_a <- function(data, title, include_guide){
 cdf_plot <- function(dt) {
   ggplot(
     data = dt,
-    aes(x = xvals, y = yy, group =idt, color = river)
+    aes(x = x, y = Fx, group =idt, color = river)
   ) + 
     geom_hline(yintercept = 0, color = "grey75") + 
     geom_vline(xintercept = 0, color = "grey75") + 
-    geom_line(alpha = 0.5) + 
+    geom_line(alpha = 0.3) + 
     scale_y_continuous(
-      name = expression(Pr(X < x)),
+      name = expression(Pr(X <= x)),
       expand = c(.05, 0),
       limits = c(0, 1)
     ) +
@@ -165,10 +174,76 @@ cdf_plot <- function(dt) {
       legend.text     = element_text(size = 8),
       axis.line.y     = element_line(color = "grey75"),
       axis.line.x     = element_blank(),
+      axis.title.y    = element_text(color = "grey25"),
+      axis.text       = element_text(color = "grey25"),
       axis.ticks      = element_line(color = "grey75")
     )
 }
 
+# Plot sample moments
+
+plot_moments <- function(dd, ss){
+  ggplot(dd,
+         aes(x = xval, y = value)) +
+    geom_hline(
+      data = ss %>% filter(river == "Baseline"),
+      aes(yintercept = mean),
+      linetype = "dotted",
+      color = "grey50"
+    ) + 
+    geom_line(
+      data = ss,
+      aes(x = xval, y = mean, group = river),
+      color = "grey10"
+    ) + 
+    geom_point(
+      data = ss,
+      aes(x = xval, y = mean),
+      color = "red",
+      shape = "triangle",
+    ) + 
+    geom_point(shape = 1, size = 0.5, color = "grey10") +
+    scale_x_continuous(
+      name  = "",
+      breaks = c(0, .66, 1, 1.33, 2, 2.33, 2.66),
+      labels = c("Baseline", "1", "2\nTuckasegee", "3", "1", "2\nLittle TN", "3") 
+    )  +
+    facet_wrap( ~ moment, ncol = 2, scales = "free_y") +
+    theme_classic() +
+    theme(
+      axis.line    = element_blank(),
+      # axis.line.x  = element_line(size = 0.5, color = "grey80"),
+      # axis.line.y  = element_line(size = 0.5, color = "grey80"),
+      axis.title.y = element_blank(),
+      axis.ticks   = element_line(color = "grey80"),
+      axis.text.x  = element_text(size = 6), 
+      axis.text.y  = element_text(size = 4), 
+      panel.border = element_rect(fill = NA, color = "grey", size = 0.3),
+      strip.background = element_blank()
+    )
+}
+
+## plot p-values
+
+plot_pvals <- function(dt){
+ dt %>%
+    mutate(
+      layer = factor(layer, levels = c("Periostracum", "Prismatic layer", "Nacre", "Nacre (annuli A)"), ordered= TRUE)
+    ) %>%
+    ggplot(data = .,
+           aes(x = -log10(p), y = layer)) +
+    geom_vline(xintercept = 0) + 
+    geom_point(size = 0.5) +
+    scale_x_continuous(
+      limits = c(0, 7)
+    ) + theme_classic() +
+    theme(
+      axis.line.x = element_line(color = "grey50", size = .5),
+      axis.line.y = element_blank(),
+      axis.title.y = element_blank(),
+      axis.ticks.y = element_blank()
+    )
+}
 
 
 
---
+++ programs/11a1_elements_by_layer.R
@@ -0,0 +1,238 @@
+#-----------------------------------------------------------------------------#
+#    Title: Summarise elemental concentrations by layer in Elktoe
+#   Author: B Saul
+#     Date: 20180119
+#  Purpose:
+#-----------------------------------------------------------------------------#
+
+library(grid)
+library(gridExtra)
+library(ggbeeswarm)
+knitr::opts_chunk$set(echo = TRUE)
+
+vers <- "V001"
+source("programs/10a_analysis_functions.R")
+
+## Collect Data ####
+
+vtransFUN <- function(x) x
+# vtransFUN <- function(x) { ifelse(x < 0, -log(-x + 1) , log(x + 1)) }
+# vtransFUN <- function(x) {log(pmax(0, x) + 1)}
+ncr_a <- elktoe_ncr(
+  .a      =  c("A"),
+  .f      = quos(n() > 10),
+  .dtrans = function(x) {max(x) - x},
+  .vtrans = vtransFUN
+) %>%
+  mutate(layer = "ncrA")
+
+ncr <- elktoe_ncr(
+  .f      = quos(n() > 10),
+  .dtrans = function(x) {max(x) - x},
+  .vtrans = vtransFUN
+) %>%
+  mutate(layer = "ncrall")
+
+psm <- elktoe_psm(
+  .f      = quos(n() > 10),
+  .dtrans = function(x) {max(x) - x},
+  .vtrans = vtransFUN
+) %>%
+  mutate(layer = "psm")
+
+pio <- elktoe_pio(
+  .f      = quos(n() > 10),
+  .dtrans = function(x) {max(x) - x},
+  .vtrans = vtransFUN
+) %>%
+  mutate(layer = "pio")
+
+dt <- bind_rows(ncr_a, ncr, psm, pio) 
+rm(ncr_a, ncr, psm, pio, valve_data)
+
+## Compute Statistics on distribution moments ####
+
+dt_moments <- dt %>%
+  group_by(layer, river, site, site_num, id, transect, element) %>% 
+  tidyr::nest() %>%
+  mutate(
+    lmom = purrr::map(data, function(x) {
+      hold <- lmom::samlmu(x$value)
+      out  <- as_tibble(hold)
+      out$moment <- names(hold)
+      out
+    })
+  ) %>%
+  select(layer, river, site, site_num, id, transect, element, lmom) %>% 
+  tidyr::unnest() %>%
+  group_by(layer, element) %>%
+  tidyr::nest() %>%
+  mutate(
+    summaries = purrr::map(
+      data, 
+      ~ .x %>% 
+        group_by(river, site, site_num, moment) %>%
+        summarise(
+          mean   = mean(value),
+          median = median(value)
+        ))
+  ) %>%
+  mutate(
+    layer_title = case_when(
+      layer == "ncrA" ~ "Nacre (annuli A)",
+      layer == "ncrall" ~ "Nacre",
+      layer == "psm"    ~ "Prismatic layer",
+      layer == "pio"    ~ "Periostracum"
+    )
+  ) %>%
+  mutate_at(
+    .vars = c("data", "summaries"),
+    .funs = funs(purrr::map(., ~ .x %>% 
+                              mutate(
+                                xval = case_when(
+                                  river == "Baseline" ~ 0,
+                                  river == "Tuckasegee" ~ .66,
+                                  TRUE ~ 2
+                                ),
+                                xval =  xval +  (site_num - 1)/3
+                              )))) %>%
+  mutate(
+    p = purrr::map2(data, summaries, ~plot_moments(.x, .y)),
+    pvals = purrr::map(
+      .x = data, 
+      .f = ~ .x %>%
+        group_by(moment) %>%
+        summarise(
+          p = kruskal.test(value, factor(site))$p.value
+        ))
+  ) 
+
+## Compute empirical CDFs ####
+
+cdf_vals <- dt %>%
+  group_by(layer, element) %>%
+  tidyr::nest() %>%
+  mutate(
+    xvals = purrr::map(data, ~ sort(unique(round(unique(.x$value), 3))))
+  ) %>%
+  mutate(
+    cdf_data = purrr::map2(
+      .x = data,
+      .y = xvals,
+      .f = function(dt, vals){
+        dt %>%
+          group_by(river, site, site_num, id, transect) %>%
+          tidyr::nest() %>%
+          mutate(
+            Fx = purrr::map(
+              data, 
+              ~ data_frame(
+                x  = vals,
+                Fx = ecdf(.x$value)(vals))
+            )
+          ) %>%
+          select(-data) %>%
+          tidyr::unnest()
+      }
+    )
+  ) %>%
+  select(-data, -xvals) %>%
+  mutate(
+    cdf_plot = purrr::map(
+      .x = cdf_data,
+      .f = ~ .x %>%
+        mutate(idt = paste0(id, transect)) %>%
+        filter(x > quantile(x, 0.025), x < quantile(x, 0.975)) %>%
+        cdf_plot()
+    )
+  )
+
+## Combine results #### 
+results <- dt_moments %>%
+  left_join(
+    cdf_vals, by = c("element", "layer")
+  ) %>% select(-data, -cdf_data)
+
+rm(dt, cdf_vals, dt_moments)
+
+## Produce output #### 
+
+results <- results %>%
+  mutate(
+    layerGrob = purrr::pmap(
+      .l = list(layer_title, p, cdf_plot),
+      .f = function(l, p, cdf){
+        txt <- textGrob(label = l, x = unit(.5, "npc"), hjust = 1, just = "left")
+        arrangeGrob(txt, cdf, p, nrow = 3, heights = c(.25, 3, 3))
+      }
+    )
+  ) %>%
+  group_by(element) %>%
+  tidyr::nest()  %>%
+  mutate(
+    pvals  = purrr::map(
+      .x = data,
+      .f = ~ purrr::map2(
+        .x$layer_title, .x$pvals, 
+        function(l, y) {
+          mutate(y, layer = l)
+        }
+      )),
+    pvals = purrr::map(pvals, ~ do.call("rbind", args = .x)),
+    pval_plot = purrr::map(pvals, ~ plot_pvals(.x)),
+    gplot = purrr::pmap(
+      .l = list(data, element, pval_plot),
+      .f = function(x, y, z){
+        ti <- textGrob(label = y)
+        tr <- arrangeGrob(grobs = x$layerGrob, ncol = 4)
+        hold <- arrangeGrob(z, ncol = 4)
+        # br <- arrangeGrob(grobs  x$poall, ncol = 4)
+        out <- arrangeGrob(ti, tr, hold, nrow = 3, heights = c(.5, 6.25, 1.5))
+      })
+  )
+
+lapply(seq_along(results$element), function(i){
+  ggsave(filename = sprintf('figures/11a1_elements_by_layer/11a1_%s_%s.pdf' , results$element[i], vers),
+         plot = results$gplot[[i]],
+         height = 7.25, width = 11, units = 'in')
+})
+
+## Summary plot ####
+
+summary_dt <- results %>%
+  select(element, pvals) %>%
+  tidyr::unnest() %>%
+  group_by(
+    element, layer
+  ) %>%
+  summarise(
+    p = min(p)
+  ) %>%
+  mutate(
+    layer = factor(layer, levels = c("Periostracum", "Prismatic layer", "Nacre", "Nacre (annuli A)"), ordered= TRUE),
+    thres = p < 0.001,
+    label = if_else(
+      p < 0.001,
+      substr(element, 1, 2),
+      ""
+    )
+  )
+
+p <- ggplot(summary_dt,
+       aes(x = -log10(p), y = layer, color = thres)) +
+  geom_beeswarm(groupOnX = FALSE, size = 0.5, shape = 1) +
+  geom_text(aes(label = label), size = 2, 
+            nudge_y = 0.1
+            # position =  position_jitter(height=0.2)
+            ) + 
+  scale_color_manual(
+    values = c("black", "red"),
+    guide  = FALSE
+  ) + 
+  theme_classic() +
+  theme(
+    axis.title.y = element_blank()
+  )
+
+ggsave(filename = sprintf('figures/11a1_elements_by_layer/11a1_summary_%s.pdf' , vers),
+       p, width = 4, height = 3)
