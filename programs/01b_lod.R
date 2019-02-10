b12ad03f498b0f41772ccf35c059792f2152a46f
bsaul <bradleysaul@gmail.com>
2019-02-10 10:02:29 -0500

    first pass at importing lower limits of detection

--- programs/01b_lod.R
+++ programs/01b_lod.R
@@ -5,37 +5,44 @@
 # Purpose: 
 #-----------------------------------------------------------------------------#
 
-## Temporary file !! ####
-recoveries <- readxl::read_excel("extdata/recoveries_example_20190202.xlsx") %>%
-  select(-Date, -Time, -"Duration(s)", -DateTime) %>%
-  select(everything(), source_file = `Source file`) 
-
-x <- recoveries %>%
-  tidyr::gather(key = "key", value = "value", -drawer, -output, -source_file, -Comments) %>%
+recoveries <- readxl::read_excel("extdata/Std recoveries comp D1-7 reproc.xlsx",
+                                 sheet = "Without Stats") %>%
   mutate(
-    standard     = stringr::str_extract(Comments, "NIST614|NIST612"),
-    standard_run = stringr::str_replace(Comments, "(NIST614|NIST612)_", ""),
+    standard     = stringr::str_extract(Comments, "614|612")
+  ) %>%
+  group_by(standard, Date) %>%
+  mutate(
+    standard_run = 1:n(),
+  ) %>%
+  ungroup() %>%
+  select(-Date, -Time, -"Duration(s)", -DateTime, -Comments) %>%
+  filter(Exclude == 0) %>%
+  select(drawer = Drawer, output = Output, source_file = `Source file`, 
+         standard, standard_run, matches("CPS|ppm"))  %>%
+  tidyr::gather(
+    key = "key", value = "value", 
+    -drawer, -output, -source_file, -standard, -standard_run,
+     na.rm = TRUE) %>%
+  mutate(
+    drawer       = stringr::str_remove(drawer, "D"),
     measure      = stringr::str_replace(stringr::str_extract(key, "_(Int2SE|LOD)$"), "_", ""),
     measure      = if_else(is.na(measure), "raw", measure),
     element      = stringr::str_replace(key, "_(Int2SE|LOD)$", "")
   ) %>%
   select(drawer, standard, output, source_file, standard_run, element, measure, value) 
 
-library(ggplot2)
-
-x %>%
-  filter(measure == "LOD", element == "Pb_ppm_m208") %>%
-  ggplot(
-    data = .,
-    aes(x = value)
-  ) + geom_histogram(binwidth = .001) + 
-  facet_grid(drawer ~ standard )
-
-
-  group_by(element, standard, drawer, measure) %>%
+lod <- recoveries %>%
+  filter(measure == "LOD") %>%
+  group_by(element, drawer, standard) %>%
   summarise(
-    mean = mean(value),
-    sd   = sd(value)
+    lod_mean = mean(value)
+  ) %>%
+  group_by(element, drawer) %>%
+  summarise(
+    lod_mean = mean(lod_mean)
   )
+
+
+
 
 valve_chemistry
