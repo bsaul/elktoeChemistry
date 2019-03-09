1f03ddfe4f26a4bd9da0d5d41f88d85f092e86e6
bsaul <bradleysaul@gmail.com>
2019-03-09 17:28:36 -0500

    adds max to L-moments

--- programs/11a0_compute_Lmoments.R
+++ programs/11a0_compute_Lmoments.R
@@ -28,31 +28,31 @@ moments_dt <- analysis_dt %>%
     nbelow = purrr::map_dbl(pwm, ~.x$numbelowthreshold),
     nobs   = purrr::map_dbl(pwm, ~.x$samplesize),
     prop_censored = nbelow/nobs,
+    max    = purrr::map_dbl(data, ~ max(.x$value)),
     lmomA = purrr::map(
       .x = pwm,
       .f = ~ lmomco::pwm2lmom(.x$Aprimebetas)),
     lmomB = purrr::map(
       .x = pwm,
       .f = ~ lmomco::pwm2lmom(.x$Bprimebetas)),
-    statsA = purrr::map2(
-      .x = lmomA,
-      .y = prop_censored,
-      function(x, y){
+    statsA = purrr::pmap(
+      .l = list(x = lmomA, y = prop_censored, z = max),
+      .f = function(x, y, z){
         tibble::tibble(
-          statistic = c("p_censored", paste0("L-moment ", 1:3)),
-          value     = c(y, x$lambdas[1:3])
+          statistic = c("p_censored", paste0("L-moment ", 1:3), "max"),
+          value     = c(y, x$lambdas[1:3], z)
         )
       }
     ),
-    statsA_ratios = purrr::map2(
-      .x = lmomA,
-      .y = prop_censored,
-      function(x, y){
+    statsA_ratios = purrr::pmap(
+      .l = list(x = lmomA, y = prop_censored, z = max),
+      .f = function(x, y, z){
         tibble::tibble(
-          statistic = c("p_censored", paste0("L-ratio ", 1:3)),
-          value     = c(y, x$ratios[2:4])
+          statistic = c("p_censored", paste0("L-ratio ", 1:3), "max"),
+          value     = c(y, x$ratios[2:4], z)
         )
       }
     ))
 
+moments_dt$statsA
 
