ff74bbf16debf45061e31be109e2c54d4cb9a930
bsaul <bradleysaul@gmail.com>
2019-03-22 09:39:41 -0400

    adds a script for adding summary variables per transect

---
+++ programs/06b_create_transect_summary_data.R
@@ -0,0 +1,34 @@
+#-----------------------------------------------------------------------------#
+#   Title: Create indicators for various analysis groupings
+#  Author: B Saul
+#    Date: 2019-03-22
+# Purpose:
+#    * agrp_any_A: id has any transect with nacre annuli A
+#    * agrp_any_B: id has any transect with nacre annuli B
+#    * agrp_first_transect: first transect of an id
+#    * agrp_first_transect_with_A: first transect of an id with nacre annuli A
+#    * agrp_first_transect_with_B: first transect of an id with nacre annuli B
+#    * agrp_first_transect_with_AB: first transect of an id with nacre annuli A AND B
+#-----------------------------------------------------------------------------#
+
+valve_data <- valve_data %>%
+  group_by(id) %>%
+  mutate(
+    agrp_any_A                  = any(A),
+    agrp_any_B                  = any(B),
+    agrp_first_transect         = (transect == min(transect)),
+    firstA_transect             = ifelse(
+      agrp_any_A, transect[min(which(A))], ""),
+    firstB_transect             = ifelse(
+      agrp_any_B, transect[min(which(B))], ""),
+    agrp_first_transect_with_A  = (transect == firstA_transect),
+    agrp_first_transect_with_B  = (transect == firstB_transect),  
+    agrp_first_transect_with_AB = (transect == firstA_transect) & (transect == firstB_transect)
+  ) %>% 
+  select(-firstA_transect, -firstB_transect)
+
+
+
+saveRDS(valve_data, file = 'data/valve_data.rds')
+
+
