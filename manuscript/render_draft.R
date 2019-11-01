9329adf7092e7c14644c175f3399d8b99cafff0e
bsaul <bradleysaul@gmail.com>
2019-11-01 08:53:28 -0400

    v012 of manuscript

--- manuscript/render_draft.R
+++ manuscript/render_draft.R
@@ -1,7 +1,6 @@
-vers <- 'v011'
+vers <- 'v012'
 dir <- 'manuscript/'
 rmarkdown::render(
   input       = paste0(dir, 'elktoe_shells.Rmd'),
-  output_file = paste0('drafts/elktoe_shells_', vers, '.pdf' )
-  )
+  output_file = paste0('drafts/elktoe_shells_', vers, '.pdf' ))
 
