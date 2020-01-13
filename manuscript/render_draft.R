409b8cf7feeb57aaf9a6b220b68948dabfe8c212
bsaul <bradleysaul@gmail.com>
2020-01-13 08:26:39 -0500

    v016

--- manuscript/render_draft.R
+++ manuscript/render_draft.R
@@ -1,6 +1,7 @@
-vers <- 'v015'
+vers <- 'v016'
 dir <- 'manuscript/'
 rmarkdown::render(
   input       = paste0(dir, 'elktoe_shells.Rmd'),
-  output_file = paste0('drafts/elktoe_shells_', vers, '.pdf' ))
-
+  output_file = I(paste0('drafts/elktoe_shells_', vers)),
+  output_format = "all"
+)
