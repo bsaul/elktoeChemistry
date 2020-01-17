a2b00683e6961e2014debfd826e0c36a14e882ea
bsaul <bradleysaul@gmail.com>
2020-01-17 08:58:50 -0500

    v017

--- manuscript/render_draft.R
+++ manuscript/render_draft.R
@@ -1,7 +1,17 @@
-vers <- 'v016'
+# Render elktoe shells manuscript
+
+vers <- 'v017'
 dir <- 'manuscript/'
 rmarkdown::render(
   input       = paste0(dir, 'elktoe_shells.Rmd'),
   output_file = I(paste0('drafts/elktoe_shells_', vers)),
   output_format = "all"
 )
+
+file.copy(from = "manuscript/elktoe_shells.docx", 
+          to = sprintf("manuscript/drafts/elktoe_shells_%s.docx", vers))
+unlink("manuscript/elktoe_shells.docx")
+
+file.copy(from = sprintf("manuscript/elktoe_shells_%s.log", vers), 
+          to = sprintf("manuscript/drafts/elktoe_shells_%s.log", vers))
+unlink(sprintf("manuscript/elktoe_shells_%s.log", vers))
