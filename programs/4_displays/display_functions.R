e60e35b56beb97c5100134fa0454b0b5d3937282
bsaul <bradleysaul@gmail.com>
2020-05-02 12:18:51 -0400

    working on plots

--- programs/4_displays/display_functions.R
+++ programs/4_displays/display_functions.R
@@ -4,6 +4,9 @@ read_all_ri_data <- function(data_dir = "data/ri"){
   files <- dir(data_dir, full.names = TRUE)
   purrr::map_dfr(
     .x = files,
-    .f = ~ readRDS(.x)
+    .f = ~ readRDS(.x) %>% 
+      dplyr::mutate(
+        p_value = purrr::map_dbl(p_value, ~ .x[[1]])
+      )
   )
 }
